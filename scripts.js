4b6f1ae910b397a64ab4b0a4bccb6b5ff6d3201c
Noeh <noehboiser221@gmail.com>
2024-11-27 23:27:22 +0800

    added mute and unmute button

--- scripts.js
+++ scripts.js
@@ -2,9 +2,8 @@ const userName = "ExcelOne-"+Math.floor(Math.random() * 100000)
 const password = "x";
 document.querySelector('#user-name').innerHTML = userName;
 
-//if trying it on a phone, use this instead...
- const socket = io.connect('https://192.168.1.18:8181/',{
-//const socket = io.connect('https://localhost:8181/',{
+// if trying it on a phone, use this instead...
+const socket = io.connect('https://192.168.1.18:8181/',{
     auth: {
         userName,password
     }
@@ -13,101 +12,92 @@ document.querySelector('#user-name').innerHTML = userName;
 const localVideoEl = document.querySelector('#local-video');
 const remoteVideoEl = document.querySelector('#remote-video');
 
-let localStream; //a var to hold the local video stream
-let remoteStream; //a var to hold the remote video stream
-let peerConnection; //the peerConnection that the two clients use to talk
+let localStream; // a var to hold the local video stream
+let remoteStream; // a var to hold the remote video stream
+let peerConnection; // the peerConnection that the two clients use to talk
 let didIOffer = false;
 
 let peerConfiguration = {
     iceServers:[
         {
             urls:[
-              'stun:stun.l.google.com:19302',
-              'stun:stun1.l.google.com:19302'
+                'stun:stun.l.google.com:19302',
+                'stun:stun1.l.google.com:19302'
             ]
         }
     ]
 }
 
-//when a client initiates a call
-const call = async e=>{
+// When a client initiates a call
+const call = async e => {
     await fetchUserMedia();
 
-    //peerConnection is all set with our STUN servers sent over
+    // peerConnection is all set with our STUN servers sent over
     await createPeerConnection();
 
-    //create offer time!
-    try{
+    // create offer time!
+    try {
         console.log("Creating offer...")
         const offer = await peerConnection.createOffer();
         console.log(offer);
         peerConnection.setLocalDescription(offer);
         didIOffer = true;
-        socket.emit('newOffer',offer); //send offer to signalingServer
-    }catch(err){
+        socket.emit('newOffer', offer); // send offer to signalingServer
+    } catch (err) {
         console.log(err)
     }
-
 }
 
-const answerOffer = async(offerObj)=>{
+const answerOffer = async(offerObj) => {
     await fetchUserMedia()
     await createPeerConnection(offerObj);
-    const answer = await peerConnection.createAnswer({}); //just to make the docs happy
-    await peerConnection.setLocalDescription(answer); //this is CLIENT2, and CLIENT2 uses the answer as the localDesc
+    const answer = await peerConnection.createAnswer({}); // just to make the docs happy
+    await peerConnection.setLocalDescription(answer); // this is CLIENT2, and CLIENT2 uses the answer as the localDesc
     console.log(offerObj)
     console.log(answer)
-    // console.log(peerConnection.signalingState) //should be have-local-pranswer because CLIENT2 has set its local desc to it's answer (but it won't be)
-    //add the answer to the offerObj so the server knows which offer this is related to
-    offerObj.answer = answer 
-    //emit the answer to the signaling server, so it can emit to CLIENT1
-    //expect a response from the server with the already existing ICE candidates
-    const offerIceCandidates = await socket.emitWithAck('newAnswer',offerObj)
-    offerIceCandidates.forEach(c=>{
+    offerObj.answer = answer;
+    // emit the answer to the signaling server, so it can emit to CLIENT1
+    const offerIceCandidates = await socket.emitWithAck('newAnswer', offerObj)
+    offerIceCandidates.forEach(c => {
         peerConnection.addIceCandidate(c);
         console.log("======Added Ice Candidate======")
     })
     console.log(offerIceCandidates)
 }
 
-const addAnswer = async(offerObj)=>{
-    //addAnswer is called in socketListeners when an answerResponse is emitted.
-    //at this point, the offer and answer have been exchanged!
-    //now CLIENT1 needs to set the remote
+const addAnswer = async(offerObj) => {
+    // addAnswer is called in socketListeners when an answerResponse is emitted.
+    // at this point, the offer and answer have been exchanged!
+    // now CLIENT1 needs to set the remote
     await peerConnection.setRemoteDescription(offerObj.answer)
-    // console.log(peerConnection.signalingState)
 }
 
-const fetchUserMedia = ()=>{
-    return new Promise(async(resolve, reject)=>{
-        try{
+const fetchUserMedia = () => {
+    return new Promise(async(resolve, reject) => {
+        try {
             const stream = await navigator.mediaDevices.getUserMedia({
                 video: true,
-                // audio: true,
+                audio: true,
             });
             localVideoEl.srcObject = stream;
-            localStream = stream;    
-            resolve();    
-        }catch(err){
+            localStream = stream;
+            resolve();
+        } catch (err) {
             console.log(err);
             reject()
         }
     })
 }
 
-const createPeerConnection = (offerObj)=>{
-    return new Promise(async(resolve, reject)=>{
-        //RTCPeerConnection is the thing that creates the connection
-        //we can pass a config object, and that config object can contain stun servers
-        //which will fetch us ICE candidates
+const createPeerConnection = (offerObj) => {
+    return new Promise(async(resolve, reject) => {
         peerConnection = await new RTCPeerConnection(peerConfiguration)
         remoteStream = new MediaStream()
         remoteVideoEl.srcObject = remoteStream;
 
-
-        localStream.getTracks().forEach(track=>{
-            //add localtracks so that they can be sent once the connection is established
-            peerConnection.addTrack(track,localStream);
+        localStream.getTracks().forEach(track => {
+            // add localtracks so that they can be sent once the connection is established
+            peerConnection.addTrack(track, localStream);
         })
 
         peerConnection.addEventListener("signalingstatechange", (event) => {
@@ -115,39 +105,32 @@ const createPeerConnection = (offerObj)=>{
             console.log(peerConnection.signalingState)
         });
 
-        peerConnection.addEventListener('icecandidate',e=>{
+        peerConnection.addEventListener('icecandidate', e => {
             console.log('........Ice candidate found!......')
-            console.log(e)
-            if(e.candidate){
-                socket.emit('sendIceCandidateToSignalingServer',{
+            if (e.candidate) {
+                socket.emit('sendIceCandidateToSignalingServer', {
                     iceCandidate: e.candidate,
                     iceUserName: userName,
                     didIOffer,
-                })    
+                })
             }
         })
-        
-        peerConnection.addEventListener('track',e=>{
+
+        peerConnection.addEventListener('track', e => {
             console.log("Got a track from the other peer!! How exciting")
-            console.log(e)
-            e.streams[0].getTracks().forEach(track=>{
-                remoteStream.addTrack(track,remoteStream);
-                console.log("Here's an exciting moment... fingers cross")
+            e.streams[0].getTracks().forEach(track => {
+                remoteStream.addTrack(track, remoteStream);
             })
         })
 
-        if(offerObj){
-            //this won't be set when called from call();
-            //will be set when we call from answerOffer()
-            // console.log(peerConnection.signalingState) //should be stable because no setDesc has been run yet
+        if (offerObj) {
             await peerConnection.setRemoteDescription(offerObj.offer)
-            // console.log(peerConnection.signalingState) //should be have-remote-offer, because client2 has setRemoteDesc on the offer
         }
         resolve();
     })
 }
 
-const addNewIceCandidate = iceCandidate=>{
+const addNewIceCandidate = iceCandidate => {
     peerConnection.addIceCandidate(iceCandidate)
     console.log("======Added Ice Candidate======")
 }
@@ -177,5 +160,27 @@ const hangup = () => {
     console.log("Call ended.");
 };
 
-document.querySelector('#call').addEventListener('click',call)
+const muteAudio = () => {
+    if (localStream) {
+        localStream.getAudioTracks().forEach(track => {
+            track.enabled = false; // Mute the audio
+        });
+    }
+    document.querySelector('#mute').style.display = "none";
+    document.querySelector('#unmute').style.display = "inline";
+};
+
+const unmuteAudio = () => {
+    if (localStream) {
+        localStream.getAudioTracks().forEach(track => {
+            track.enabled = true; // Unmute the audio
+        });
+    }
+    document.querySelector('#mute').style.display = "inline";
+    document.querySelector('#unmute').style.display = "none";
+};
+
+document.querySelector('#call').addEventListener('click', call);
 document.querySelector('#hangup').addEventListener('click', hangup);
+document.querySelector('#mute').addEventListener('click', muteAudio);
+document.querySelector('#unmute').addEventListener('click', unmuteAudio);
